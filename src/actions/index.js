088e4de204367871f2436f731acab1ee5d572316
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-06 16:12:56 -0500

    added everything upto tables

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ export const FETCH_WEATHER="FETCH_WEATHER";
 
 export function fetchWeather(city){
 	const url = `${ROOT_URL}&q=${city},ca`;
+	
 
 	const request=axios.get(url);
 	return {
--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/searchbar';
+import WeatherList from '../containers/weatherlist';
+
 export default class App extends Component {
   render() {
     return (
       <div>React simple starter
       	<SearchBar />
+      	<WeatherList />
       </div>
       
     );
