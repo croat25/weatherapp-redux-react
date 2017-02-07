c9ca03808c44464bd35778c3559f5d81313fdf24
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-07 09:01:59 -0500

    started updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,1 @@
 
-import axios from 'axios';
-
-const API_KEY='71040a5de64fcacdc35968adb04254e9';
-const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-
-export const FETCH_WEATHER="FETCH_WEATHER";
-
-export function fetchWeather(city){
-	const url = `${ROOT_URL}&q=${city},ca`;
-	
-
-	const request=axios.get(url);
-	return {
-
-		type: FETCH_WEATHER,
-		payload:request
-	};
-}
--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import SearchBar from '../containers/searchbar';
-import WeatherList from '../containers/weatherlist';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      	<SearchBar />
-      	<WeatherList />
+      React simple start
       </div>
       
     );
